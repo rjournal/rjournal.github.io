ef7071ea9650196c5ae6f4ec88ad51f16df0d90b
hadley <h.wickham@gmail.com>
2013-08-16 12:17:34 -0500

    Update publish script

--- publish.sh
+++ publish.sh
@@ -1,2 +1,12 @@
-jekyll
-rsync -ai --delete _site/ ../web-svn/html/
+# Assume there's a web-svn directory, which is a git svn checkout
+# of https://svn.r-project.org/Rjournal/trunk
+
+jekyll build
+
+# from http://nathangrigg.net/2012/04/rsyncing-jekyll/
+rsync -i --recursive --delete --checksum _site/ ../web-svn/html/
+
+cd ../web-svn
+git add -A .
+git commit -m"Update site"
+git svn dcommit
