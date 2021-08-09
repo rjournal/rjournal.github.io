81abf8c9e89466e6d1c145f3aeade9edc19a1687
mitchelloharawild <mitch.ohara-wild@monash.edu>
2021-08-09 23:39:58 +1000

    Seemingly working lua filter

--- _web/news/RJ-2021-1-editorial/latex-pkg.lua
+++ _web/news/RJ-2021-1-editorial/latex-pkg.lua
@@ -1,32 +1,47 @@
-local cranpkg = {
-    html = 'https://cran.r-project.org/package=%1',
-    pdf = '\\CRANpkg{%s}'
+local replace_cranpkg = {
+  match = '\\CRANpkg%{(%w+)%}',
+  html = 'https://cran.r-project.org/package=%s',
+  pdf = '\\CRANpkg{%s}'
 }
-local biopkg = {
-    html = 'https://www.bioconductor.org/packages/%1/',
-    pdf = '\\BIOpkg{%s}'
+local replace_biopkg = {
+  match = '\\BIOpkg%{(%w+)%}',
+  html = 'https://www.bioconductor.org/packages/%s/',
+  pdf = '\\BIOpkg{%s}'
 }
-local pkg = {
-    html = '%1',
-    pdf = '\\pkg{%s}'
+local replace_pkg = {
+  match = '\\pkg%{(%w+)%}',
+  html = '%s',
+  pdf = '\\pkg{%s}'
 }
-
 
 function RawInline(elem)
   local out = {}
   
-  local pkg = string.match(elem.text, '\\pkg%{(%w+)%}.*')
-  
+  local cranpkg = string.match(elem.text, replace_cranpkg.match)
+  local biopkg = string.match(elem.text, replace_biopkg.match)
+  local pkg = string.match(elem.text, replace_pkg.match)
   if pkg ~= nil then
---     return pandoc.Para{pandoc.Strong(pkg)}
-      
     if FORMAT:match 'html.*' then
-        table.insert(out, pandoc.Link(pkg, '#'))
+      table.insert(out, pandoc.Link(pkg, '#'))
     else
-        table.insert(out, pandoc.Str(string.gsub(elem.text, '%@pkg%((%w+)%)', pkg.cranpkg)))
+      table.insert(out, pandoc.RawInline(string.format(replace_pkg.pdf, pkg)))
     end
-  else
-    out = elem
+  elseif cranpkg ~= nil then
+    if FORMAT:match 'html.*' then
+      table.insert(out, pandoc.Link(cranpkg, string.format(replace_cranpkg.html, cranpkg)))
+    else
+      table.insert(out, pandoc.RawInline(string.format(replace_cranpkg.pdf, cranpkg)))
+     end
+  elseif biopkg ~= nil then
+    if FORMAT:match 'html.*' then
+      table.insert(out, pandoc.Link(biopkg, string.format(replace_biopkg.html, biopkg)))
+    else
+      table.insert(out, pandoc.RawInline(string.format(replace_biopkg.pdf, biopkg)))
+    end
+  end
+  
+  if #(out) == 0 then
+    return elem
   end
   return out
 end
