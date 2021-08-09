81cd2dd611af86e8c3705f7010b66b4eea895b0f
mitchelloharawild <mitch.ohara-wild@monash.edu>
2021-08-09 22:50:18 +1000

    Updated 2021-1 editorial

---
+++ _news/RJ-2021-1-editorial/latex-pkg.lua
@@ -0,0 +1,29 @@
+local cranpkg = {
+    html = 'https://cran.r-project.org/package=%1',
+    pdf = '\\CRANpkg{%s}'
+}
+local biopkg = {
+    html = 'https://www.bioconductor.org/packages/%1/',
+    pdf = '\\BIOpkg{%s}'
+}
+local pkg = {
+    html = '%1',
+    pdf = '\\pkg{%s}'
+}
+
+function RawBlock(elem)
+  local out = {}
+  
+  local pkg = string.match(elem.text, '\\pkg%((%w+)%)')
+  
+  if pkg ~= nil then
+    if FORMAT:match 'html.*' then
+        table.insert(out, pandoc.Link(pkg, '#'))
+    else
+        table.insert(out, pandoc.Str(string.gsub(elem.text, '%@pkg%((%w+)%)', pkg.cranpkg)))
+    end
+  else
+    out = elem
+  end
+  return out
+end
