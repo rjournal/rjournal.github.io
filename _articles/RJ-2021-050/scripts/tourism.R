f42105f7c814132bb75c0d80e37ed300eb9bf9d5
mitchelloharawild <mitch.ohara-wild@monash.edu>
2022-11-11 09:21:29 +1100

    Republish RJ-2021-050 with some revisions

--- _articles/RJ-2021-050/scripts/tourism.R
+++ _articles/RJ-2021-050/scripts/tourism.R
@@ -1,17 +1,21 @@
 ## ---- tourism-shared
 library(tsibble)
 library(tsibbletalk)
-tourism_shared <- tourism_monthly %>% 
+library(dplyr)
+tourism_shared <- tourism_monthly %>%
+  # Comment out the next line to run the full example
+  filter(State %in% c("Tasmania", "Western Australia")) %>%
+  mutate(Region = stringr::str_replace(Region, "Australia's ", "WA's ")) %>%
   as_shared_tsibble(spec = (State / Region) * Purpose)
 
 ## ---- plotly-key-tree
-p_l <- plotly_key_tree(tourism_shared, height = 1100, width = 800)
+p_l <- plotly_key_tree(tourism_shared, height = 800, width = 800)
 
 ## ---- tourism-series
 library(ggplot2)
 p_tr <- tourism_shared %>%
   ggplot(aes(x = Month, y = Trips)) +
-  geom_line(aes(group = Region), alpha = .5, size = .4) +
+  geom_line(aes(group = Region), alpha = .5, linewidth = .4) +
   facet_wrap(~ Purpose, scales = "free_y") +
   scale_x_yearmonth(date_breaks = "5 years", date_labels = "%Y")
 
@@ -27,8 +31,8 @@ p_br <- tourism_feat %>%
 library(plotly)
 subplot(p_l,
   subplot(
-    ggplotly(p_tr, tooltip = "Region", width = 1100),
-    ggplotly(p_br, tooltip = "Region", width = 1100),
+    ggplotly(p_tr, tooltip = "Region", width = 700),
+    ggplotly(p_br, tooltip = "Region", width = 700),
     nrows = 2),
   widths = c(.4, .6)) %>%
   highlight(dynamic = TRUE)
--- _articles/RJ-2021-050/tsibbletalk.R
+++ _articles/RJ-2021-050/tsibbletalk.R
@@ -4,42 +4,45 @@ library(knitr)
 opts_chunk$set(
   echo = FALSE, warning = FALSE, message = FALSE, comment = "#>",
   fig.path = 'figure/', fig.align = 'center', fig.show = 'hold',
-  cache = TRUE, cache.path = 'cache/',
   out.width = ifelse(is_html_output(), "100%", "\\textwidth")
 )
 opts_knit$set(root.dir = here::here())
+
+
+## ----external-----------------------------------------------------------------
 read_chunk("scripts/demo.R")
 read_chunk("scripts/tourism.R")
 read_chunk("scripts/pedestrian.R")
 
 
-## ---- load-pkgs---------------------------------------------------------------
+## ----load-pkgs----------------------------------------------------------------
 library(feasts)
 library(tsibble)
 library(tsibbledata)
+library(patchwork)
 
 
 ## ---- print-retail------------------------------------------------------------
 print(aus_retail, n = 5)
 
 
-## ----highlight-retail, fig.height = 6, out.width = ".49\\linewidth", fig.show = "hold", fig.cap = "Plots for the \\code{aus\\_retail} data, with the series of strongest seasonal strength highlighted. (a) An overlaid time series plot. (b) A scatter plot drawn from their time series features, where each dot represents a time series from (a).", fig.subcap = c("", ""), fig.ncol = 2----
+## ----highlight-retail, fig.height = 3.6, fig.show = "hold", fig.cap = "Plots for the \\code{aus\\_retail} data, with the series of strongest seasonal strength highlighted. (a) An overlaid time series plot. (b) A scatter plot drawn from their time series features, where each dot represents a time series from (a)."----
 library(tidyverse)
 library(ggrepel)
 library(gghighlight)
 
-aus_retail %>%
+a <- aus_retail %>%
   as_tibble() %>% # gghighlight issue for group_by() + filter()
   mutate(group = paste(State, ":", Industry)) %>%
   ggplot(aes(x = Month, y = Turnover)) +
   geom_line(aes(group = group)) +
   gghighlight(
     State == "Queensland", Industry == "Department stores",
-    label_params = list(vjust = -2)
+    label_params = list(vjust = 0, nudge_y = 1, label.size = 0.15)
   ) +
   scale_x_yearmonth(breaks = yearmonth(c("1990 Jan", "2000 Jan", "2010 Jan")))
 
-aus_retail %>%
+b <- aus_retail %>%
   features(Turnover, feat_stl) %>%
   mutate(group = paste(State, ":", Industry)) %>%
   ggplot(aes(x = trend_strength, y = seasonal_strength_year)) +
@@ -49,25 +52,32 @@ aus_retail %>%
     State == "Queensland", Industry == "Department stores",
     use_direct_label = FALSE
   ) +
-  geom_label_repel(aes(label = group), vjust = 2, nudge_x = -0.1)
+  geom_label_repel(aes(label = group), vjust = 0.4,
+                   nudge_x = -0.1, label.size = 0.15)
+
+a+b
 
 
 ## ----tourism-shared, echo = TRUE----------------------------------------------
 library(tsibble)
 library(tsibbletalk)
-tourism_shared <- tourism_monthly %>% 
+library(dplyr)
+tourism_shared <- tourism_monthly %>%
+  # Comment out the next line to run the full example
+  filter(State %in% c("Tasmania", "Western Australia")) %>%
+  mutate(Region = stringr::str_replace(Region, "Australia's ", "WA's ")) %>%
   as_shared_tsibble(spec = (State / Region) * Purpose)
 
 
-## ----tourism-linking-fig, fig.cap = "Snapshot of exploring an ensemble of linked plots of the Australian tourism data, built on a \\code{tourism\\_shared} object. It also illustrates persistent linked brushing to compare two groups."----
+## ----tourism-linking-fig, fig.cap = "Snapshot of exploring an ensemble of linked plots of the Australian tourism data, built on a \\code{tourism\\_shared} object. It also illustrates persistent linked brushing to compare two groups.", eval = knitr::is_latex_output()----
 include_graphics("img/tourism-linking.png")
 
 
 ## ----plotly-key-tree, echo = TRUE---------------------------------------------
-p_l <- plotly_key_tree(tourism_shared, height = 1100, width = 800)
+p_l <- plotly_key_tree(tourism_shared, height = 800, width = 800)
 
 
-## ----tourism-series, echo = TRUE, eval = FALSE--------------------------------
+## ----tourism-series, echo = TRUE, eval = knitr::is_html_output()--------------
 #> library(ggplot2)
 #> p_tr <- tourism_shared %>%
 #>   ggplot(aes(x = Month, y = Trips)) +
@@ -76,7 +86,7 @@ p_l <- plotly_key_tree(tourism_shared, height = 1100, width = 800)
 #>   scale_x_yearmonth(date_breaks = "5 years", date_labels = "%Y")
 
 
-## ----tourism-scatter, echo = TRUE, eval = FALSE-------------------------------
+## ----tourism-scatter, echo = TRUE, eval = knitr::is_html_output()-------------
 #> library(feasts)
 #> tourism_feat <- tourism_shared %>%
 #>   features(Trips, feat_stl)
@@ -85,21 +95,25 @@ p_l <- plotly_key_tree(tourism_shared, height = 1100, width = 800)
 #>   geom_point(aes(group = Region), alpha = .8, size = 2)
 
 
-## ----tourism-multi, echo = TRUE, eval = FALSE---------------------------------
+## ----tourism-multi, layout="l-body-outset", echo = TRUE, eval = knitr::is_html_output(), fig.cap = "Exploring an ensemble of linked plots of the Australian tourism data, built on a \\code{tourism\\_shared} object. Click one of the nodes in the hierarchical tree to enable persistent linked brushing to compare two groups. Points and lines can also be selected in other plots. (Only Western Australia and Tasmania are included for the interactive plot in the html version, so size reasons.)"----
 #> library(plotly)
 #> subplot(p_l,
 #>   subplot(
-#>     ggplotly(p_tr, tooltip = "Region", width = 1100),
-#>     ggplotly(p_br, tooltip = "Region", width = 1100),
+#>     ggplotly(p_tr, tooltip = "Region", width = 700),
+#>     ggplotly(p_br, tooltip = "Region", width = 700),
 #>     nrows = 2),
 #>   widths = c(.4, .6)) %>%
 #>   highlight(dynamic = TRUE)
 
 
-## ----wrap-ped, fig.show = "hold", fig.cap = "Snapshots wrapping after slicing the \\code{pedestrian20} data at different intervals, (a) none, (b) daily and (c) weekly. This type of interaction is made possible with Shiny elements.", fig.subcap = c("Initial overview state", "1-day state", "7-day state, anchoring to Monday"), fig.ncol = 1----
+## ----wrap-ped, fig.show = "hold", fig.cap = "Snapshots wrapping after slicing the \\code{pedestrian20} data at different intervals, (a) none, (b) daily and (c) weekly. This type of interaction is made possible with Shiny elements.", fig.subcap = c("Initial overview state", "1-day state", "7-day state, anchoring to Monday"), fig.ncol = 1, eval = knitr::is_latex_output()----
 include_graphics("img/wrap-0.png")
 include_graphics("img/wrap-1.png")
 include_graphics("img/wrap-7.png")
+
+
+## ----wrap-ped-html, fig.show = "hold", fig.cap = "Animations showing wrapping after slicing the \\code{pedestrian20} data at different intervals, (a) none, (b) daily and (c) weekly. This type of interaction is made possible with Shiny elements.", fig.subcap = c("Initial overview state", "1-day state", "7-day state, anchoring to Monday"), fig.ncol = 1, eval = knitr::is_html_output()----
+#> include_graphics("img/shiny-wrap.gif")
 
 
 ## ----load-ped, eval = FALSE---------------------------------------------------
@@ -143,3 +157,5 @@ include_graphics("img/wrap-7.png")
 #>   "rbokeh", "leaflet", "crosstalk", "loon")
 #> write_bib(pkgs, "rpkgs.bib")
 
+```{.r .distill-force-highlighting-css}
+```
