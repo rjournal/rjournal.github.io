c7d90a713f09b6870858b12058cf739d0f0a10f9
dicook <dicook@users.noreply.github.com>
2022-11-25 05:10:20 +0000

    deploy: e5ebb6eea082c1564e0df990b13e5e26b590a80a

--- articles/RJ-2021-050/scripts/demo.R
+++ articles/RJ-2021-050/scripts/demo.R
@@ -2,6 +2,7 @@
 library(feasts)
 library(tsibble)
 library(tsibbledata)
+library(patchwork)
 
 ## ---- print-retail
 print(aus_retail, n = 5)
@@ -11,18 +12,18 @@ library(tidyverse)
 library(ggrepel)
 library(gghighlight)
 
-aus_retail %>%
+a <- aus_retail %>%
   as_tibble() %>% # gghighlight issue for group_by() + filter()
   mutate(group = paste(State, ":", Industry)) %>%
   ggplot(aes(x = Month, y = Turnover)) +
   geom_line(aes(group = group)) +
   gghighlight(
     State == "Queensland", Industry == "Department stores",
-    label_params = list(vjust = -2)
+    label_params = list(vjust = 0, nudge_y = 1, label.size = 0.15)
   ) +
   scale_x_yearmonth(breaks = yearmonth(c("1990 Jan", "2000 Jan", "2010 Jan")))
 
-aus_retail %>%
+b <- aus_retail %>%
   features(Turnover, feat_stl) %>%
   mutate(group = paste(State, ":", Industry)) %>%
   ggplot(aes(x = trend_strength, y = seasonal_strength_year)) +
@@ -32,5 +33,8 @@ aus_retail %>%
     State == "Queensland", Industry == "Department stores",
     use_direct_label = FALSE
   ) +
-  geom_label_repel(aes(label = group), vjust = 2, nudge_x = -0.1)
+  geom_label_repel(aes(label = group), vjust = 0.4,
+                   nudge_x = -0.1, label.size = 0.15)
+
+a+b
 
