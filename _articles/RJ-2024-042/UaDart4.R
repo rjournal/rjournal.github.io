c0e92fc465a7dc0fb9c33fbf96d93161e9512ae6
Mark van der Loo <mark.vanderloo@gmail.com>
2025-07-17 15:46:40 +0200

    fixed figure width issue in latex

--- _articles/RJ-2024-042/UaDart4.R
+++ _articles/RJ-2024-042/UaDart4.R
@@ -11,7 +11,7 @@ library(UpAndDownPlots)
 head(data.frame(AutoSalesX))
 
 
-## ----CPI, echo = FALSE, fig.asp = 0.6, fig.cap="UK Consumer Price Index changes August 2017 to August 2018.  The red dotted line shows the overall percentage change.  The rectangles with blue borders show the changes of the main sectors.  Heights are percentage changes, widths are index values in August 2017, so that the areas of the rectangles are each group contributions to the overall change.  The grey bars show the percentage and absolute changes for the individual components."----
+## ----CPI, echo = FALSE, fig.asp = 0.6,out.width='90%', fig.cap="UK Consumer Price Index changes August 2017 to August 2018.  The red dotted line shows the overall percentage change.  The rectangles with blue borders show the changes of the main sectors.  Heights are percentage changes, widths are index values in August 2017, so that the areas of the rectangles are each group contributions to the overall change.  The grey bars show the percentage and absolute changes for the individual components."----
 library(UpAndDownPlots)
 yp <- ud_prep(CPIuk, weight="Weight", v1="Aug2017", v2="Aug2018",
   levs=c("Sector", "Component"), sortLev=c("orig", "orig"))
@@ -19,13 +19,13 @@ yd <- ud_plot(yp, drawFrom="SmallToBig")
 yd$uad
 
 
-## ----CPIlab, echo = FALSE, fig.asp = 1.2, fig.cap="CPI changes ordered by percentage changes of sectors and by percentage changes of components within sectors.  Two sectors, Transport and Recreation and Culture, made the biggest contributions to change.  Prices increased for all components in most sectors."----
+## ----CPIlab, echo = FALSE, fig.asp = 1.2,out.width='90%', fig.cap="CPI changes ordered by percentage changes of sectors and by percentage changes of components within sectors.  Two sectors, Transport and Recreation and Culture, made the biggest contributions to change.  Prices increased for all components in most sectors."----
 yq <- ud_prep(CPIuk, weight="Weight", v1="Aug2017", v2="Aug2018", levs=c("Sector", "Component"), sortLev=c("perc", "perc"))
 yf <- ud_plot(yq, labelvar="Sector", drawFrom="SmallToBig", vscale=c(-30, 30))
 yf$uadl
 
 
-## ----NIvars, echo = TRUE, fig.cap="Percentage changes in population between 2011 and 2017 by age, gender, district, and area."----
+## ----NIvars, echo = TRUE,out.width='90%', fig.cap="Percentage changes in population between 2011 and 2017 by age, gender, district, and area."----
 library(gridExtra)
 g4 <- dgroup(NIpop, byvars=c("age", "gender", "LGD2014_name", "area_name"),
              v1="y2011", v2="y2017")
@@ -51,14 +51,14 @@ kables(list(kK1, kK2), caption="Absolute and percentage changes in N. Ireland po
 kables(list(kK3, kK4))
 
 
-## ----NIag, echo = TRUE, fig.cap="Percentage changes in population between 2011 and 2017 by age and then gender (males above, females below)."----
+## ----NIag, echo = TRUE,out.width='90%', fig.cap="Percentage changes in population between 2011 and 2017 by age and then gender (males above, females below)."----
 ag <- ud_prep(NIpop, v1="y2011", v2="y2017", levs=c("age", "gender"),
               sortLev=c("orig", "perc"))
 kag <- ud_plot(ag, labelvar="age")
 kag$uadl
 
 
-## ----NIdag, echo = TRUE, fig.cap="Percentage changes in population between 2011 and 2017 for three districts (changes outlined in blue), four age groups (outlined in brown), and gender (filled bars)."----
+## ----NIdag, echo = TRUE,out.width='90%', fig.cap="Percentage changes in population between 2011 and 2017 for three districts (changes outlined in blue), four age groups (outlined in brown), and gender (filled bars)."----
 NIpopW <- NIpop %>% filter(LGD2014_name %in% c("Belfast", "Derry City and Strabane",
                 "Newry, Mourne and Down"))
 zdag <- ud_prep(NIpopW, v1="y2011", v2="y2017", levs=c("LGD2014_name", "age", "gender"),
@@ -67,7 +67,7 @@ zdag2 <- ud_plot(zdag, labelvar="LGD2014_name", drawFrom="SmallToBig")
 zdag2$uadl
 
 
-## ----autoS, echo = FALSE, fig.width = 6, fig.asp = 1, fig.cap="Compact car sales changes in Germany by manufacturer between 2017 and 2018.  The plot on the left is drawn with a baseline of zero change and ordered by percentage changes.  The plot on the right is drawn with a baseline of the overall market change and ordered by changes in market share."----
+## ----autoS, echo = FALSE, fig.width = 6, fig.asp = 1,out.width='90%', fig.cap="Compact car sales changes in Germany by manufacturer between 2017 and 2018.  The plot on the left is drawn with a baseline of zero change and ordered by percentage changes.  The plot on the right is drawn with a baseline of the overall market change and ordered by changes in market share."----
 library(patchwork)
 AutoSalesXcomp <- AutoSalesX %>% filter(Segment=="Compact")
 yxp <- ud_prep(AutoSalesXcomp, v1="sales17", v2="sales18",
